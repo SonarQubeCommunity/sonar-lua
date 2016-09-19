a7897b1d7d529569de8c36fae0393e3545c550bb
Fatiahmadi <fati.ahmadi66@gmail.com>
2016-09-20 01:38:02 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mvn license:format
+
 mvn clean install
+mvn license:format
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   mvn sonar:sonar -Dsonar.login=$SONAR_TOKEN
