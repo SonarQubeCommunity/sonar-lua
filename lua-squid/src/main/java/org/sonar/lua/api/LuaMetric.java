a48864d45b6997ba48d3ea3c5b0be8b6fce487e6
Fatiahmadi <fati.ahmadi66@gmail.com>
2016-10-01 15:35:15 +0200

    update squid

--- lua-squid/src/main/java/org/sonar/lua/api/LuaMetric.java
+++ lua-squid/src/main/java/org/sonar/lua/api/LuaMetric.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Lua Plugin
- * Copyright (C) 2016 SonarSource SA
- * mailto:fati.ahmadi66 AT gmail DOT com
+ * Copyright (C) 2013-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package org.sonar.lua.api;
 
 
