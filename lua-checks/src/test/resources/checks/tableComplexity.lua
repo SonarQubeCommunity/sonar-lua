e7944e5f4612e07bd2681995081b8b79a0770576
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 23:18:20 +0200

    Add files via upload

--- lua-checks/src/test/resources/checks/tableComplexity.lua
+++ lua-checks/src/test/resources/checks/tableComplexity.lua
@@ -1,13 +1,2 @@
-test={key=std.tostring(key or 'nil'),id=curRow[fi.id] }
-
--- index is one complexity,
---std.+
---tostring()+
---[fi.id]++ two indexes
---table +
-
-
-
-
-
+test={key=std.tostring(key or 'nil'),id=curRow[fi.id] }--or is one complexity
 
