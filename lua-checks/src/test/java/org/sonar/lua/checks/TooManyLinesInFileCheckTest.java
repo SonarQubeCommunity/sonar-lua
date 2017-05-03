28c1bdac19918a0129e13645b1209fe8fece9261
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 23:27:51 +0200

    Add files via upload

---
+++ lua-checks/src/test/java/org/sonar/lua/checks/TooManyLinesInFileCheckTest.java
@@ -0,0 +1,45 @@
+/*
+ * SonarQube Lua Plugin
+ * Copyright (C) 2013-2016-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.lua.checks;
+import java.io.File;
+
+
+import org.junit.Test;
+import org.sonar.lua.LuaAstScanner;
+import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.squidbridge.checks.CheckMessagesVerifier;
+
+public class TooManyLinesInFileCheckTest {
+
+
+  @Test
+  public void test_positive() {
+	  TooManyLinesInFileCheck check = new TooManyLinesInFileCheck();
+	    check.maximum = 2;
+    SourceFile file = LuaAstScanner.scanSingleFile(new File("src/test/resources/checks/tooManyLinesInFile.lua"), check);
+    CheckMessagesVerifier.verify(file.getCheckMessages())
+      .next().withMessage(
+        "File \"tooManyLinesInFile.lua\" has 5 lines, which is greater than 2 authorized. Split it into smaller files.")
+      .noMore();
+  }
+
+ 
+
+}
