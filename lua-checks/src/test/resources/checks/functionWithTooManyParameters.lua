ec18da04a960d26ef325a2e7d84eeb482c9cc511
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 22:31:36 +0200

    Add files via upload

---
+++ lua-checks/src/test/resources/checks/functionWithTooManyParameters.lua
@@ -0,0 +1,20 @@
+ DoTheJob = function(t1,t2,t3,t4,t5,t6,t7,p8) 
+  lnlog.traceinfo("addToSet", {set=std.tostring(set or 'nil'), key=std.tostring(key or 'nil')})
+  set[key] = 'true'
+         
+end
+
+function test(t1,t2,t3,t4,t5,t6,t7,p8)
+print()
+end
+local function test(t1,t2,t3,t4,t5,t6,t7,p8,p9,...)
+print()
+end
+
+
+
+
+
+
+
+
