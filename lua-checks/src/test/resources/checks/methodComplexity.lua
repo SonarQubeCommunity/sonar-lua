cbc93946dbee0b08c3e685a1b62a4c2c70872901
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 23:02:57 +0200

    Add files via upload

---
+++ lua-checks/src/test/resources/checks/methodComplexity.lua
@@ -0,0 +1,14 @@
+ function func2(a, b, c)--Method
+  local function fun3()--violation
+   function func1()--Method
+      function func1()--Method
+      
+       return result
+       end 
+       end
+   end 
+   q=function ()
+      function other()--Method
+     end
+      end
+        end
