b3eccd8582bd1dde369436a314656878c47cf950
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 19:32:07 +0200

    Update NestedControlFlowDepthCheck.java

--- lua-checks/src/main/java/org/sonar/lua/checks/NestedControlFlowDepthCheck.java
+++ lua-checks/src/main/java/org/sonar/lua/checks/NestedControlFlowDepthCheck.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Lua Plugin
  * Copyright (C) 2016 SonarSource SA
- * mailto:contact AT DOT com
+ * mailto:fati.ahmadi66 AT gmail DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
