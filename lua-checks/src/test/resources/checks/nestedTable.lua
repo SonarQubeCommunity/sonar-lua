cf9072ed67bc888861e13dccf9afaa06f28a63cb
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 23:15:48 +0200

    Add files via upload

---
+++ lua-checks/src/test/resources/checks/nestedTable.lua
@@ -0,0 +1,12 @@
+polyline = {color="blue", thickness=2, npoints=4,
+                 {x=0,
+                 {a,
+                 {c--should be broken
+               }},y=0},
+                 {x=-10, y=0},
+                 {x=-10, y=1},
+                 {x=0,   y=1}
+               }
+               
+               
+ 
