8b2fdd7afd408a47d40178f1ddf7fbd3af28a232
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-03 22:09:31 +0200

    Add files via upload

---
+++ lua-checks/src/test/resources/checks/functionCallComplexity.lua
@@ -0,0 +1,12 @@
+create(function (a,b)--function
+   if a>0 and b>0 then--if , and
+      print(a+b)
+    if a>b then--if
+       print(a)
+    if a<b then--if
+       print("finished")
+       end
+     end
+    end
+  end)
+ 
