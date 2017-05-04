1f9c8db57a4b69e48d6ee36e12f6260c48c37aaf
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-04 02:26:25 +0200

    Update LuaKeyword.java

--- lua-squid/src/main/java/org/sonar/lua/api/LuaKeyword.java
+++ lua-squid/src/main/java/org/sonar/lua/api/LuaKeyword.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Lua Plugin
- * Copyright (C) 2013-2016-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
+ * Copyright (C) 2016 
+ * mailto:fati.ahmadi AT gmail DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
