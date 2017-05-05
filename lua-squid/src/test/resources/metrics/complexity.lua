377881094e98f17c040cb05ff160217458a57303
Fatiahmadi <fati.ahmadi66@gmail.com>
2017-05-05 02:50:46 +0200

    Update complexity.lua

--- lua-squid/src/test/resources/metrics/complexity.lua
+++ lua-squid/src/test/resources/metrics/complexity.lua
@@ -7,11 +7,4 @@ function foo (a)--+2
      end
      
 ---     
- co = coroutine.create(function (a,b)--4
-           print("co-body", a, b)
-           local r = foo(a+1)
-           print("co-body", r)
-           local r, s = coroutine.yield(a+b, a-b)
-               print("co-body", r, s)
-            return b, "end"
-     end)    
+ 
