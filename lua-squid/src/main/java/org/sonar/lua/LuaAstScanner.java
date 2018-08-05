eb86065c76aca8dcd93a3bd1323d4a5e30a328a9
Fatiahmadi <fati.ahmadi66@gmail.com>
2018-08-05 19:59:51 +0200

    Update LuaAstScanner.java

--- lua-squid/src/main/java/org/sonar/lua/LuaAstScanner.java
+++ lua-squid/src/main/java/org/sonar/lua/LuaAstScanner.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube Lua Plugin
- * Copyright (C) 2013-2016-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
+ * Copyright (C) 2016 
+ * mailto:fati.ahmadi66 AT gmail DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
